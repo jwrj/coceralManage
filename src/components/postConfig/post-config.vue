7b20baba68f41dd66ca83035b9ec64f275beb78e
小伍 <35486827+wuguanyao@users.noreply.github.com>
2018-09-25 17:25:11 +0800

    新增组件

---
+++ src/components/postConfig/post-config.vue
@@ -0,0 +1,227 @@
+<template>
+	
+	<div>
+		
+		<Tree :data="data" :render="renderContent"></Tree>
+		
+	</div>
+	
+</template>
+
+<script>
+
+export default {
+	name: '',
+	components:{//组件模板
+	},
+	props:{//组件道具（参数）
+		/* ****属性用法*****
+		 * 
+		 * 传递类型 type: Array | Number | String | Object
+		 * 为必传 required: true
+		 * 默认值 default: ''
+		 * 
+		 */
+	},
+    data () {//数据
+        return {
+        	
+        	data: [
+        		{
+        			title: 'parent 1',
+                    expand: true,
+                    render: (h, { root, node, data }) => {
+                        return h('span', {
+                            style: {
+                                display: 'inline-block',
+                                width: '100%',
+                                border: '1px solid red',
+                                cursor: 'pointer',
+                            },
+                            class: 'ivu-tree-title'
+                        }, [
+                            h('span', [
+                                h('Icon', {
+                                    props: {
+                                        type: 'ios-folder-outline'
+                                    },
+                                    style: {
+                                        marginRight: '8px'
+                                    }
+                                }),
+                                h('span', data.title)
+                            ]),
+                            h('span', {
+                                style: {
+                                    display: 'inline-block',
+                                    float: 'right',
+                                    marginRight: '32px',
+                                }
+                            }, [
+                                h('Button', {
+                                    props: Object.assign({}, this.buttonProps, {
+                                        icon: 'ios-add',
+                                        type: 'primary'
+                                    }),
+                                    style: {
+                                        width: '64px'
+                                    },
+                                    on: {
+                                        click: () => {
+                                        	this.append(data);
+                                        }
+                                    }
+                                })
+                            ])
+                        ]);
+        			}
+                }
+        	],
+            buttonProps: {
+                type: 'default',
+                size: 'small',
+            }
+        	
+        }
+    },
+    methods: {//方法
+    	
+    	renderContent (h, { root, node, data }) {
+            return h('span', {
+                style: {
+                    display: 'inline-block',
+                    width: '100%'
+                }
+            }, [
+                h('span', [
+                    h('Icon', {
+                        props: {
+                            type: 'ios-paper-outline'
+                        },
+                        style: {
+                            marginRight: '8px'
+                        }
+                    }),
+                    h('span', data.title)
+                ]),
+                h('span', {
+                    style: {
+                        display: 'inline-block',
+                        float: 'right',
+                        marginRight: '32px'
+                    }
+                }, [
+                	h('Button', {
+                        props: Object.assign({}, this.buttonProps, {
+                            icon: 'md-create'
+                        }),
+                        style: {
+                            marginRight: '8px'
+                        },
+                        on: {
+                            click: () => {
+                            	this.edit();
+                            	console.log(root);
+                            	console.log(node);
+                            	console.log(data);
+                            }
+                        }
+                    }),
+                    h('Button', {
+                        props: Object.assign({}, this.buttonProps, {
+                            icon: 'ios-add'
+                        }),
+                        style: {
+                            marginRight: '8px'
+                        },
+                        on: {
+                            click: () => { this.append(data) }
+                        }
+                    }),
+                    h('Button', {
+                        props: Object.assign({}, this.buttonProps, {
+                            icon: 'ios-remove'
+                        }),
+                        on: {
+                            click: () => { this.remove(root, node, data) }
+                        }
+                    })
+                ])
+            ]);
+        },
+        edit(){
+        	
+        },
+        append (data) {
+            const children = data.children || [];
+            children.push({
+                title: 'appended node',
+                expand: true
+            });
+            this.$set(data, 'children', children);
+        },
+        remove (root, node, data) {
+            const parentKey = root.find(el => el === node).parent;
+            const parent = root.find(el => el.nodeKey === parentKey).node;
+            const index = parent.children.indexOf(data);
+            parent.children.splice(index, 1);
+        }
+    	
+    },
+    computed: {//计算属性
+        	
+    },
+    watch: {//监测数据变化
+    	
+	},
+    
+    //===================组件钩子===========================
+    
+    created () {//实例被创建完毕之后执行
+    	
+	},
+    mounted () {//模板被渲染完毕之后执行
+    	
+	},
+	
+	//=================组件路由勾子==============================
+	
+	beforeRouteEnter (to, from, next) {//在组件创建之前调用（放置页面加载时请求的Ajax）
+		
+		(async() => {//执行异步函数
+			
+			//async、await错误处理
+			try {
+				
+				/*
+				 * 
+				 * ------串行执行---------
+				 * console.log(await getAjaxData());
+				 * ...
+				 * 
+				 * ---------并行：将多个promise直接发起请求（先执行async所在函数），然后再进行await操作。（执行效率高、快）----------
+				 * let abc = getAjaxData();//先执行promise函数
+				 * ...
+				 * console.log(await abc);
+				 * ...
+				*/
+				next(vm => {
+					
+				});
+				
+			} catch(err) {
+				console.log(err);
+			}
+			
+		})();
+		
+	},
+	
+}
+</script>
+
+<style scoped lang="less">
+	.abc{
+		
+	}
+</style>
