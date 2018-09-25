cc99c7d09f3ad660356eda89598156ebe7da6125
pxy0113 <43558746+pxy0113@users.noreply.github.com>
2018-09-25 10:11:36 +0800

    添加了会员管理

--- src/router/router.js
+++ src/router/router.js
@@ -14,15 +14,6 @@ import secondaryView from '@/components/secondary-view.vue';
 
 export default [
 	{
-	    path: '/login',
-	    name: 'login',
-	    meta: {
-	      title: 'Login - 登录',
-	      hideMenu: true,
-	    },
-	    component: () => import('@/views/testPages/page2.vue')
-  	},
-	{
 		path: '/',
 		name: '_home',
 	    redirect: '/home',
@@ -43,98 +34,23 @@ export default [
 		    }
 		]
 	},
-	//一级路由
 	{
-		path: '/a',
-		name: 'a',
+		path: '/user',
+		name: 'user',
 		meta: {
-			hideBreadcrumb: true,//面包屑导航中隐藏
-		},
-	    component: Main,
-		children: [
-			{
-				path: 'a1',
-				name: 'a1',
-				meta: {
-					title: '一级菜单1-1',
-					icon: 'md-alarm',
-				},
-				component: () => import('@/views/testPages/page1.vue')
-			}
-		]
-	},
-	//二级路由
-	{
-		path: '/b',
-		name: 'b',
-		meta: {
-			title: '二级菜单111',
+			title: '会员管理',
 			icon: 'md-alarm',
 		},
-	    component: Main,
+		component: Main,
 		children: [
 			{
-				path: 'b1',
-				name: 'b1',
+				path: 'joinChamber',
+				name: 'joinChamber',
 				meta: {
-					title: '1-1',
+					title: '申请加入商会',
 					icon: 'md-alarm',
 				},
-				component: () => import('@/views/testPages/page1.vue')
-			},
-			{
-				path: 'b2',
-				name: 'b2',
-				meta: {
-					title: '1-2',
-					icon: 'md-alarm',
-				},
-				component: () => import('@/views/testPages/page2.vue')
-			},
-			{
-				path: 'b3',
-				name: 'b3',
-				meta: {
-					title: '1-3',
-					icon: 'ios-paper',
-					hideMenu: true,
-					highlightName: 'b2',
-				},
-				component: () => import('@/views/testPages/page1.vue')
-			}
-		]
-	},
-	//三级路由
-	{
-		path: '/c',
-		name: 'c',
-		meta: {
-			title: '二级菜单2',
-			icon: 'md-alarm',
-			showAlways: true,
-		},
-	    component: Main,
-		children: [
-			{
-				path: 'c1',
-				name: 'c1',
-				meta: {
-					title: '2-1',
-					icon: 'md-alert',
-					//showAlways: true,
-				},
-				component: secondaryView,
-				children: [
-					{
-						path: 'c1-1',
-						name: 'c1-1',
-						meta: {
-							title: '三级菜单1',
-							icon: 'md-alarm',
-						},
-						component: () => import('@/views/testPages/page3.vue')
-					},
-				]
+				component: () => import('@/views/user/joinChamber.vue')
 			},
 		]
 	},
---
+++ src/views/main/user/joinChamber.vue
@@ -0,0 +1,55 @@
+<template>
+	
+	<div>
+		
+		
+		
+	</div>
+	
+</template>
+
+<script>
+
+export default {
+	name: 'joinChamber',
+	components:{//组件模板
+	},
+	props:{//组件道具（参数）
+		/* ****属性用法*****
+		 * 
+		 * 传递类型 type: Array | Number | String | Object
+		 * 为必传 required: true
+		 * 默认值 default: ''
+		 * 
+		 */
+	},
+    data () {//数据
+        return {
+        	
+        }
+    },
+    methods: {//方法
+    	
+    },
+    computed: {//计算属性
+        	
+    },
+    watch: {//监测数据变化
+    	
+	},
+    
+    //===================组件钩子===========================
+    
+    created () {//实例被创建完毕之后执行
+    	
+	},
+    mounted () {//模板被渲染完毕之后执行
+    	
+	},
+	
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
