46ef30ba411bbfb746d346355b87eef515443d9a
pxy0113 <43558746+pxy0113@users.noreply.github.com>
2018-10-12 11:45:25 +0800

    aaa

--- src/views/price/payment.vue
+++ src/views/price/payment.vue
@@ -2,30 +2,34 @@
 
 	<div id="payment">
 		<Card>
-			<h1 slot="title">缴费情况</h1>
-			<div class="content" style="display: flex;">
-				<div class="left" style="text-align: right;font-weight: 700;">
+			<h1 slot="title">会员信息</h1>
+			<Row>
+				<Col span="4" style="text-align: right;">
 					<p>姓名：</p>
 					<p>岗位：</p>
 					<p>届次：</p>
-					<p>届次时间：</p>
-					<p>截至时间：</p>
-					<p>岗位会费：</p>
-				</div>
-				<div class="right">
+				</Col>
+				<Col span="6">
 					<p>{{username}}</p>
 					<p>{{work}}</p>
 					<p style="color: red;">{{post_num}}</p>
+				</Col>
+				<Col span="4" style="text-align: right;">
+					<p>届次时间：</p>
+					<p>截至时间：</p>
+					<p>岗位会费：</p>
+				</Col>
+				<Col span="6">
 					<p>{{post_from}}</p>
 					<p>{{post_to}}</p>
 					<p>{{price}}</p>
-				</div>
-			</div>
+				</Col>
+			</Row>
 		</Card>
 		<Card style="margin-top: 16px;">
 			<div slot="title" class="title">
 				<h1>缴费记录</h1>
-				<Button class="btnSmall" size="small" type="primary" @click="addRecord">+添加条目</Button>
+				<Button style="margin-left: 10px;" size="small" type="primary" @click="addRecord">添加缴费记录</Button>
 			</div>
 			<table-list :tableColumns="tableColumns" :tableData="tableData">
 
@@ -47,26 +51,25 @@
 					<Form :model="paying" ref="paying" :rules="ruleValidate" :label-width="80">
 
 						<FormItem label="缴费类别:" prop="kind">
-
-							<Select v-model="paying.kind" placeholder="请选择缴费类别" style="width: 200px;">
-								<Option value="both">全部</Option>
-								<Option value="work">岗位会费</Option>
-								<Option value="special">特殊会费</Option>
-								<Option value="fund">基金会费</Option>
-							</Select>
+    <RadioGroup v-model="paying.kind" type="button">
+        <Radio :label="item" v-for="item of list" ></Radio>
+    </RadioGroup>
+						</FormItem>
+						<FormItem label="届次:" prop="jc">
+							<Input v-model="paying.jc" style="width: 237px;"></Input>
 						</FormItem>
 						<FormItem label="金额:" prop="price">
-							<Input style="width: 200px;" v-model="paying.price">
+							<Input style="width: 237px;" v-model="paying.price">
 							<span slot="append">元</span>
 							</Input>
 						</FormItem>
 						
 						<FormItem label="时间:" prop="time">
-							<DatePicker type="date" @on-change="dateChange" :value="paying.time"></DatePicker>
+							<DatePicker type="date" @on-change="dateChange" :value="paying.time"style="width: 237px;"></DatePicker>
 						</FormItem>
 						
 						<FormItem label="缴纳方式:" prop="way">
-							<Select v-model="paying.way" placeholder="请选择" style="width: 200px;">
+							<Select v-model="paying.way" placeholder="请选择方式" style="width: 237px;">
 								<Option value="wayOne">现金缴纳</Option>
 								<Option value="wayTwo">对公转账</Option>
 								<Option value="wayThree">基金冲减</Option>
@@ -110,11 +113,13 @@
 				post_to: '2019-1-1',
 				price: '100元',
 				pay: 'both',
+				list:['特殊会费','岗位会费','基金会费'],
 				paying: {
 					price: '',
-					kind: '',
+					kind: '特殊会费',
 					way: '',
-					time: ''
+					time: '',
+					jc:''
 				},
 				modalShow: false,
 				tableColumns: [{
@@ -189,7 +194,20 @@
 						required: true,
 						message: '请选择方式',
 						trigger: 'blur'
-					}]
+					}],
+					jc: [{ 
+						required: true, 
+						message: '请输入届次', 
+						trigger: 'blur' ,
+						},
+					{ 
+						type: 'number', 
+						message: '请输入数字格式', 
+						trigger: 'blur', 
+						transform(value) {
+							return Number(value);
+						}}
+					]
 				},
 			}
 		},
@@ -206,7 +224,6 @@
 				this.modalShow = true;
 				
 			},
-			
       		handleSubmit(name) {
       			
 				this.$refs[name].validate((valid) => {
@@ -234,10 +251,14 @@
       		
 		},
 		computed: { //计算属性
-
+　　pokerHistory() {
+　　　　return this.paying.kind
+　　}
 		},
 		watch: { //监测数据变化
-
+        pokerHistory(newValue, oldValue) {
+　　　　console.log(newValue)
+　　}
 		},
 
 		//===================组件钩子===========================
